f3672bad1b531bec63821f2d4f94436bd390e07a
NastuzziSamy <samy@nastuzzi.fr>
2019-05-10 12:33:31 +0200

    Config project
    Signed-off-by: NastuzziSamy <samy@nastuzzi.fr>

--- config/app.php
+++ config/app.php
@@ -174,11 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        /*
-         * We need to load here our providers
-         */
-        Laramore\Providers\ModelLoader::class,
     ],
 
     /*
